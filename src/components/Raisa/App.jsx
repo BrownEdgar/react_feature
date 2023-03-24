32bad07c8a42b0d6c901e989f0306c510ea37da0
Sisak <128540608+PoghosyanSisak@users.noreply.github.com>
2023-03-24 21:17:05 +0400

    Add files via upload

--- src/components/Raisa/App.jsx
+++ src/components/Raisa/App.jsx
@@ -1,26 +1,7 @@
-import React, { useState } from "react";
+import React from 'react'
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
-  const addClick = () => {
-    setCount(count + 1);
-  };
-
-  const subtractClick = () => {
-    setCount(count - 1);
-  };
-
-  const nullClick = () => {
-    setCount(0);
-  };
-
-  return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={addClick}>To Add</button>
-      <button onClick={subtractClick}>To Subtract</button>
-      <button onClick={nullClick}>To Null</button>
-    </div>
-  );
+	return (
+		<div>App</div>
+	)
 }
