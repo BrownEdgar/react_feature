e8e48db6a5325362ec3245fb65d98c57ba368a78
Khaninyan <davkhaninyan@gmail.com~>
2023-03-25 01:56:03 +0400

    added first homework example

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./components/Raisa/App";
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import App from './components/Taron/App';
+import App from './components/Davit/App';
 
-
->>>>>>> 7c19c03ecf823d2ba1e396973999f40e2090d949
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
