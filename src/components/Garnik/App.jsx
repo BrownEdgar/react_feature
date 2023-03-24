7ab2ee879231c5609cfa248cda4f598d1790f501
Mamikyan-G <garnikmamikyan9@gmail.com>
2023-03-24 20:34:05 +0400

    react tnayin1

--- src/components/Garnik/App.jsx
+++ src/components/Garnik/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function App() {
+	
+	const [arr,setArr] = useState(["A","B","C"])
+
+	const handleClick = () => {
+		const copy = [...arr]
+		copy.push("D")
+	setArr(copy)
+	}
 	return (
-		<div>App</div>
+		<div>
+			<h1>React Button: {JSON.stringify(arr)}</h1>
+			<button onClick={handleClick}>Click me</button>
+		</div>
 	)
 }
