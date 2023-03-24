bb6ce2c04d960883fb4340e5e95cbc9282d69411
HakobyanTaron <taron.hakobyan333@gmail.com>
2023-03-24 19:32:51 +0400

    added first homework example

--- src/components/Taron/App.jsx
+++ src/components/Taron/App.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function App() {
+export default function App() { 
+	const [count, setCount] = useState(1)
+
+
+	const handleFirstClick = () =>{
+		setCount(count + 1)
+	}
+
+	const handleSecondClick = () =>{
+		setCount(count - 1)
+	}
+
+	const handleThirdClick = () =>{
+		setCount(0)
+	}
+
 	return (
-		<div>App</div>
+		<div>
+			<h1>{count}</h1>
+			<div className='box'>
+				<button onClick={handleFirstClick}>+</button>
+				<button onClick={handleSecondClick}>-</button>
+				<button onClick={handleThirdClick}>AC</button>
+			</div>
+		</div>
 	)
 }
+
+
+// sarqel mi hat component vortex ka mek hat tiv ev erek hat kochak meky +1 a anum meky -1 a anum , meknel beruma zroyakani
