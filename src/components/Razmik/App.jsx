89734db3c8e7d8f4e6fc38fb851e46b7ecd6a7a3
Razmik23 <razmik.gasparyam21@gmail.com>
2023-03-24 14:45:59 +0400

    Homework

--- src/components/Razmik/App.jsx
+++ src/components/Razmik/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function App() {
+	
+	const[arr,setArr]=useState(["A","B","C"])
+
+	const handleClick = ()=>{
+		const copy = [...arr]
+		copy.push("d") 
+		setArr(copy)
+	}
 	return (
-		<div>App</div>
+		<div>
+			<h1>react Button:{JSON.stringify(arr)}</h1>
+			<button onClick ={handleClick}>Click me</button>
+		</div>
 	)
 }
