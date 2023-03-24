ff7ac595c80dbd32b10af8e6230a0f07c73768f7
raiskhachatryan <raiskhachatryan@gmail.com>
2023-03-24 21:10:31 +0400

    Homework: Create 3 button

--- src/components/Raisa/App.jsx
+++ src/components/Raisa/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function App() {
-	return (
-		<div>App</div>
-	)
+  const [count, setCount] = useState(0);
+
+  const addClick = () => {
+    setCount(count + 1);
+  };
+
+  const subtractClick = () => {
+    setCount(count - 1);
+  };
+
+  const nullClick = () => {
+    setCount(0);
+  };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={addClick}>To Add</button>
+      <button onClick={subtractClick}>To Subtract</button>
+      <button onClick={nullClick}>To Null</button>
+    </div>
+  );
 }
