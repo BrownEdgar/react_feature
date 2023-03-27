56c4d70b787984835064b47ebb7202701179e07f
ArusikMamajanyan <128699526+ArusikMamajanyan@users.noreply.github.com>
2023-03-27 22:57:26 +0400

    2nd homework

--- src/components/Arusik/workWithImages/App.jsx
+++ src/components/Arusik/workWithImages/App.jsx
@@ -1,14 +1,25 @@
-import React , { useState } from 'react'
+import React, { useState } from 'react'
+import data from "./data.json"
+import Product from './Product/Product'
+
+import style from "./App.module.css" 
 
 export default function App() {
-  const [url, setUrl] = useState("https://images.unsplash.com/photo-1663860705042-2de106ec7aff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDV8Ym84alFLVGFFMFl8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60")
+  const [products, setProducts] = useState(data)
+
+  const handleDelete = (id) => {
+    const f = products.filter(product => product.id != id)
+    setProducts(f)
+  }
   return (
-    <div>
-      {/* variant 1 */}
-      {/* <img src="https://images.unsplash.com/photo-1663860705042-2de106ec7aff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDV8Ym84alFLVGFFMFl8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="" /> */}
-      {/* variant 2 */}
-      <img src={url} alt="" />
-    
+    <div className={style.container}>
+      {products.length >0
+      ? (
+        products.map((elem) => {
+          return <Product product={elem} key={elem.id} handleDelete={handleDelete} />
+        })
+      )
+    : <h1>No data:</h1>}
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import App from './components/Edgar/App';
+import App from './components/Arusik/workWithImages/App.jsx';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
