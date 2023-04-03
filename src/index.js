2d694cfa7b541251ee533071a1e779356899df81
Khaninyan <davkhaninyan@gmail.com~>
2023-04-03 16:38:49 +0400

    Posts

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-<<<<<<< HEAD
-import App from './components/test/App'
-=======
-import App from './components/Taron/Hooks/useEffect/App.jsx';
 
->>>>>>> 946428bf5a956a0994e9ec53dcdd9be84058ec64
+import App from './components/Davit/Posts/App';
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
