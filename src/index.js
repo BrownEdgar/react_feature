44c4fc275f1e4de10b93f0aa14f7b835da16f245
raiskhachatryan <raiskhachatryan@gmail.com>
2023-04-04 18:53:22 +0400

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-
-import App from './components/Davit/Modal/App';
-
+import App from "./components/Raisa/HW_Users/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
