16d1c11db0e470df6e7de0893ca3551101402e2d
Razmik23 <razmik.gasparyam21@gmail.com>
2023-04-06 13:06:51 +0400

    Form_Homework

--- src/components/Razmik/ReactForm/App.jsx
+++ src/components/Razmik/ReactForm/App.jsx
@@ -1,27 +1,77 @@
 import React,{useState} from 'react'
 import './App.scss'
 
+let count = 0;
+
 export default function App() {
-    const [formData,setFormData] = useState({
-        username:""
-    })
+    const [users,setUsers] = useState([])
+    const [notification, setNotification] = useState(false)
+    const [error, setError] = useState(false)
+        
     const handleSubmit = (e) =>{
         e.preventDefault();
-        const {username} = e.target
+        const {name,email} = e.target
+        const user = {
+            id: ++count,
+            name: name.value,
+            email:email.value
+        }
+        if(users.length === 0 ){
+            setUsers([user])
+        }else{
+            setUsers([...users,user])
+        }
+        setNotification(true);
+        setTimeout(setNotification,2000,false)
+        
      }
-     const handleChange = ()=>{
-        setFormData({username:e.target.value});
+     const handleBlur = (e)=>{
+        const {value:email} = e.target
+        const emailExist = users.some(user => user.email.toLowerCase() === email.toLowerCase());
+        if(emailExist){
+            setError(true)
+        }else{
+            setError(false)
+        }
      }
-  return (
-    <div>
-        <form>
-            <div>
-                <label htmlFor="username">username</label>
-                <input type="text" name = 'username' id = 'username' onChange={handleChange}/>
+    return (
+    <div className='App'>
+        <div className={`alert ${notification?'show':''}`}>
+            <p>User Saved Successfily!</p>
+        </div>
+       <div className='content'>
+            <h1>Contact Us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ducimus doloremque perferendis illum labore quisquam placeat itaque assumenda odio fuga sint, atque consectetur quod rerum!</p>
+        </div> 
+       <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="Name"></label>
+                <input 
+                    type="text" 
+                    name = 'name' 
+                    id = 'name'
+                    required 
+                    placeholder='Name'/>
             </div>
             <div>
-                <input type="submit" name = 'save' />
+                <label htmlFor="Email"></label>
+                <input 
+                    type="text" 
+                    name = 'email' 
+                    id = 'email'
+                    required 
+                    placeholder='Email'
+                    onBlur={handleBlur}
+                />
+                {error && <p className='error'>This email is allredy exist</p>}
             </div>
+
+            <div>
+                <input 
+                    type="submit" 
+                    value ='Send' />
+            </div>
+        
         </form>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-<<<<<<< HEAD
-import App from './components/Sisak/HomeWork4-Form/App'
-=======
-import App from "./components/Davit/ContactUS/App";
->>>>>>> 801b2ddd4ac552cb3831a415c099a599f82ba64b
+ import App from './components/Razmik/ReactForm/App'
+ 
+// <<<<<<< HEAD
+// import App from './components/Sisak/HomeWork4-Form/App'
+// =======
+// import App from "./components/Davit/ContactUS/App";
+// >>>>>>> 801b2ddd4ac552cb3831a415c099a599f82ba64b
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
