abc0bbc673f7b64b861e28fb7c7b55cdf560e5c7
PoghosyanSisak <poghosyansisak@gmail.com>
2023-04-11 01:27:39 +0400

    added Homework6

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import App from "./components/Davit/ReactForm/Responsive/App"
+import App from "./components/Sisak/Homework6/App"
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
