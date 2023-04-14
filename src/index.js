7b683f783a66598dfb1b6cee39d00246da0b30d1
Khaninyan <davkhaninyan@gmail.com~>
2023-04-15 01:49:09 +0400

    prev homework

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import {BrowserRouter as Router} from "react-router-dom"
 
-import App from './components/Taron/React-router-6/App.jsx';
+import App from './components/Davit/Reacctr-router-6/App';
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-	<Router>
+	<Router >
 		<App />
 	</Router>
 );
