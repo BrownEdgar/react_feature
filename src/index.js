714963b38e970f0e1b604fef8cdee116e1f7b799
BrownEdgar <nikogosjanedgar@gmail.com>
2023-04-15 20:36:02 +0400

    added useContext basics examples

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import {BrowserRouter as Router} from "react-router-dom"
 
-import App from './components/Edgar/Hooks/useReducer/App';
+import App from './components/Edgar/Hooks/useContext/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
