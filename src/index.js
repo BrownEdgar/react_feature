d47adc95a661493b7719531c62289377d801cfe7
Razmik23 <razmik.gasparyam21@gmail.com>
2023-04-18 17:18:42 +0400

    Reducer_Homework

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
-
-import App from "./components/Razmik/Reaccrt-router-6/App"
-
-
-
+// import App from "./components/Razmik/Hooks/useReducer/App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
+	
 root.render(
-<Router>
-    <App />
-</Router>);
+	<Router>
+    	<App />
+	</Router>);
