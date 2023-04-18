80f0df6588e0ed846de1235d772ce8d8e13bcb16
BrownEdgar <nikogosjanedgar@gmail.com>
2023-04-18 19:26:41 +0400

    added new branch example

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
  
 
-import App from './components/Edgar/Test/App';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
