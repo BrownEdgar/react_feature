fa2230779d62c12a1cc920867ecac493bb42bd5e
BrownEdgar <nikogosjanedgar@gmail.com>
2023-04-22 18:43:37 +0400

    add data

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
  
 
-import App from './components/Edgar/Test/App';
+import App from './components/Edgar/Hooks/useMemo/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 	
 root.render(
-
 		<Router>
 			<App />
 		</Router>
