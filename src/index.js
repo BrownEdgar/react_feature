cc43184ca548073421528ceabcdfdc7b2d98be84
Rubenners <rubennersesyan00@gmail.com>
2023-04-23 17:41:21 +0400

    Edgar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-<<<<<<< HEAD
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './components/Ruben/React-router-6/App';
-=======
 import {BrowserRouter as Router} from "react-router-dom"
 
-import App from './components/Taron/React-router-6/App.jsx';
->>>>>>> 44a6de8b60cffe9bc8db00b01c0215890b4ac369
+import App from './components/Ruben/Hooks/useReducer/App';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
