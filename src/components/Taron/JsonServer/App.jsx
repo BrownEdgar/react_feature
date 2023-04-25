ef1dd74c465fc6892733ed2bbba8a0f0d19a334f
BrownEdgar <nikogosjanedgar@gmail.com>
2023-04-25 20:31:47 +0400

    added custom hook example

--- src/components/Taron/JsonServer/App.jsx
+++ src/components/Taron/JsonServer/App.jsx
@@ -13,7 +13,6 @@ export default function App() {
 		setPosts(response.data)
 	}
 	useEffect(() => {
-
 		getPosts()
 	}, [])
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from 'react-router-dom'
 import "./index.css";
 
-import App from './components/Edgar/jsonServer/App';
-
-
+import App from './components/Edgar/Hooks/CustomHook/App';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
