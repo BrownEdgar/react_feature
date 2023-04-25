f2bf666da79902c5e1a93db3c27bfef402a65363
Razmik23 <razmik.gasparyam21@gmail.com>
2023-04-25 16:23:53 +0400

    json-server

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import {BrowserRouter as Router} from "react-router-dom"
 
-import App from './components/Razmik/json-server/App';
+//import App from './components/Razmik/json-server/App';
 
 
 
- import App from "./components/Razmik/Hooks/useMemo/App"
+ 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
