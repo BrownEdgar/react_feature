1098ee82452e81a67a8e2118899069f90f489393
ArusikMamajanyan <128699526+ArusikMamajanyan@users.noreply.github.com>
2023-04-27 16:18:21 +0400

    homework

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from 'react-router-dom'
 import "./index.css";
 
-import App from './components/Edgar/jsonServer/App';
+import App from './components/Arusik/Hooks/CustomHookHomework/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
