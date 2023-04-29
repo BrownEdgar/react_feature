fddfe2762c60b168c0c7e981328a25a6066ae488
Khaninyan <davkhaninyan@gmail.com~>
2023-04-29 17:59:29 +0400

    pagination

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from 'react-router-dom'
 import "./index.css";
 
-import App from './components/Davit/Hooks/CustomHook/App';
+import App from './components/Davit/Pagination/App';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
