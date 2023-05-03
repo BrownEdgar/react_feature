771d5f3f9a2c63db4be8a7c6830e5d9fce34641f
Razmik23 <razmik.gasparyam21@gmail.com>
2023-05-03 22:19:47 +0400

    firsttime redux

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,16 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 export default function App() {
-const {count,data,posts,person} = useSelector(state => state)
-
-
+const {count,posts,data,person} = useSelector(state => state)
 const dispatch = useDispatch()
 const handleClick = () => {
     dispatch({type:'add-count'})
 }
 const handleClick1 = ()=>{
-  dispatch({type:'add-d',payload:{item:'d'}})
+  dispatch({type:'add-angular',payload:{item:'angular'}})
 }
 const  handleClick2 =()=>{
   dispatch({type:'change-age',payload:{age:31}})
-}
-const handleClick3 = ()=>{
-  dispatch({type:'find-six'})
 }
 useEffect(()=>{
   axios('https://jsonplaceholder.typicode.com/posts?_limit=4')
@@ -29,14 +24,13 @@ useEffect(()=>{
   return (
     <div>
       <h1>React-redux  count:{count}</h1>
-    <pre>
-        {JSON.stringify(posts,null,1)}
-      </pre>
+      <pre>{JSON.stringify(posts,null,1)}</pre>
+      <pre>{JSON.stringify(data,null,1)}</pre>
       <h2>{JSON.stringify(person,null,1)}</h2>
       <button onClick={handleClick}>add count</button>
-      <button onClick ={handleClick1}>Add 'd'</button>
+      <button onClick = {handleClick1}>Add Angular</button>
       <button onClick = {handleClick2}>Change Age</button>
-      <button onClick={handleClick3}>filter</button>
+      
     </div>
   )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { createStore } from "redux"
 
 const initialState = {
     count:0,
-    data:[1,42,63,555,36,643],
+    data:["Reactjs","javascript","sass"],
     person:{
         id:1,
         name:'hovnatan',
@@ -10,21 +10,15 @@ const initialState = {
     },
     posts:[]
 }
-function foo(state){
- const result = state.data.map(n => n.toString())
- .filter(elem=>elem.startsWith('6')).length;
- return {...state,count:result}
-}
 function rootReducer(state,action){
     switch(action.type){
         case 'add-count':return {...state,count:state.count + 1};
-        case 'add-d': return {...state,data:state.data.concat(action.payload.item)}
+        case 'add-angular': return {...state,data:state.data.concat(action.payload.item)}
         case 'add-posts':return {
             ...state,
             posts:action.payload.posts
         }
         case 'change-age':return{...state,person:{...state.person,age:action.payload.age}}
-        case 'find-six':return foo(state)
         default:return state
     }
 }
