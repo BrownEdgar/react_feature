09eac8b7ca497159d8dcce67d8a5a726f1a7874f
ArusikMamajanyan <128699526+ArusikMamajanyan@users.noreply.github.com>
2023-05-11 18:43:00 +0400

    homework

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
-import App from "./components/Raisa/HomeWorks/Paginate/App";
+import App from "./components/Arusik/FetchApi/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
